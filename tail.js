980c0679790af3fae4f30eef60e1de2ae5f3419b
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-14 21:06:45 +0000

    added tail function that returns the tail end of an array except for the first element

---
+++ tail.js
@@ -0,0 +1,22 @@
+// a function that compares actual value to expected value and returns a pass or fail message
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🦖🦖🦖 Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  //let length = array.length-1;
+  let result = array.slice(1);
+  //return result;
+  console.log(result);
+};
+let testArray = ['light', 'cat', 'dog'];
+tail(testArray);
+console.log(testArray);
+assertEqual(testArray.length, 3);
+let singleItemArray = ['kitten'];
+tail(singleItemArray);
+assertEqual(singleItemArray.length, 1);
