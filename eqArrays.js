378f2b5bdd07a666ff01d9f25080624610349c6b
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-15 23:48:58 +0000

    evaluating equality of array elementsand returning pass or fail messages

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+// a function that compares actual value to expected value and returns a pass or fail message
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/* ======================================================= */
+
+let eqArrays = function(array1, array2){
+  console.log(array1,array2);
+  for(let i=0; i< array1.length; i++){
+    if(array1[i]!==array2[i]){
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
