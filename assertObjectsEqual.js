4bd3fc49f4c543e170ff997ec329b500882c08e5
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-16 23:58:32 +0000

    takes in two objects {expected object} {actual object} and evaluates their  equality by printing PASS and FAIL msgs - Also added additional if statement to eqObjects that checks of objects keys are arrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,3 @@
-/*==========================================================
-FUNCTION = assertEqual
-a function that compares actual value to expected value and returns a pass or fail message
-===========================================================*/
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log("\x1b[31m%s\x1b[0m", `⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 /*==========================================================
 FUNCTION = eqObjects
@@ -22,10 +10,6 @@ const eqObjects = function(object1, object2) {
   if (Array.isArray(object1) === Array.isArray(object2)) {
     let obj1Keys = Object.keys(object1).sort(); //stores sorted object keys
     let obj2Keys = Object.keys(object2).sort();
-    //console.log('Keys of obj2 ====== ' , obj2Keys);
-    //console.log('Keys of obj1 ====== ' ,obj1Keys);
-    
-    //console.log('length 1: ' + obj1Length + 'length 2: ' + obj2Length);
     // checks the objects have the same amount of keys if not return false and exit
     if (obj1Keys.length !== obj2Keys.length) {
       return false;
@@ -33,27 +17,40 @@ const eqObjects = function(object1, object2) {
     for (let x = 0; x < obj1Keys.length; x++) { // loops through the keys in obj1
       for (let i = 0; i < obj2Keys.length; i++) { //loops through the keys in obj2
         if (obj1Keys[x] === obj2Keys[i]) { // match keys in both objects
-          //console.log('This is the keys of Second object: ' + obj2Keys[i]);
-          //console.log('This is the keys of First object: ' + obj1Keys[x]);
           result = true; //assign value of true to result
         } else {
           result = false; // if the key values don't match return false
         }
       }
     }
-    //console.log(result);
     return result;
   } else {
     return false;
   }
 };
+
+/*==========================================================
+FUNCTION = assertObjectsEqual
+takes in two objects {expected object} {actual object} and evaluates their
+ equality by printing PASS and FAIL msgs
+===========================================================*/
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= built in utility function
+  // console.log(`Actual Object: ${inspect(actual)}`);
+  // console.log(`Expected Object: ${inspect(expected)}`);
+  if (eqObjects(actual, expected)) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)} `);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `⛔️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-//console.log('this is ba object' + ba);
-eqObjects(ab, ba); // => true
+assertObjectsEqual(ab, ba);
 
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-
-assertEqual(eqObjects(ab, ba), true);// pass
-assertEqual(eqObjects(ab, abc), false);// fail
+/*
+  console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `⛔️ Assertion Failed: ${actual} !== ${expected}`);
+*/
