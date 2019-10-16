8c4efbf25e348dc25aaeccd4fe618f87323cce0e
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-16 00:23:00 +0000

    adjusted return location

--- eqArrays.js
+++ eqArrays.js
@@ -14,10 +14,9 @@ let eqArrays = function(array1, array2){
   for(let i=0; i< array1.length; i++){
     if(array1[i]!==array2[i]){
       return false;
-    } else {
-      return true;
     }
   }
+  return true;
 };
 
 // eqArrays([1, 2, 3], [1, 2, 3]) // => true
