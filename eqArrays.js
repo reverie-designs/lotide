618deca6a42554aa13b4a2a073b62e9f275437c3
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-21 16:26:13 +0000

    seperated eqArrays instructions with test cases and used module

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-// a function that compares actual value to expected value and returns a pass or fail message
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log("\x1b[31m%s\x1b[0m", `⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/* ======================================================= */
 
 let eqArrays = function(array1, array2){
   console.log(array1,array2);
@@ -19,12 +9,4 @@ let eqArrays = function(array1, array2){
   return true;
 };
 
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+module.exports = eqArrays;
