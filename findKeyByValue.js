81e18f31f3f3ea8cab983213b876b88e430bd757
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-21 22:04:51 +0000

    added test to findKeyByValue function and removed test cases from the original file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -12,12 +12,3 @@ const findKeyByValue = function(obj, value) {
 };
 
 module.exports = findKeyByValue;
-
-// const bestTVShowsByGenre = {
-//   'sci_fi': "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama:  "The Wire"
-// };
-// console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
