5e7373ea92d3dcca8f280ab85b2be2472b1847fe
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-21 22:51:38 +0000

    added test files for eqArrays and deleted test cases from original file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+describe("#eqArrays", ()=>{
+  it('array1 of numbers is equal to array 2 of same numbers returns true', ()=>{
+    let array1 =[1, 2, 3];
+    let array2 = [1, 2, 3];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
+  it('array1 with numerical values is equal to array 2 with reversed numerical values returns false', ()=>{
+    let array1 =[1, 2, 3];
+    let array2 = [3, 2, 1];
+    assert.deepEqual(eqArrays(array1, array2), false);
+  });
+  it('array1 with string values is equal to array 2 with same string values returns true', ()=>{
+    let array1 =["1", "2", "3"];
+    let array2 = ["1", "2", "3"];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
+});
