13ce25c80dd6098bdc20285a84f0073c335802ad
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-21 17:39:09 +0000

    implemented mocha for middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,19 @@
 const middle = require('../middle');
-const assertArrayEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-//middle([1,2,9,8,7,9,10,11,12]); //7
-//middle([1, 2, 3, 4, 5, 6]); //[3,4]
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns an empty array if array only contains one item", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns an empty array if input contains only two items", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
